de403df31cc46488f72ba7d7691325e02d43695f
SENAN111 <behbudovsenan0@gmail.com>
2022-12-06 01:24:57 +0400

    first commit

---
+++ Models/People.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate.Models
+{
+    internal class People
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public string Name 
+        {
+            get
+            { 
+                return name; 
+            } 
+            set 
+            { 
+                     if(value.Trim().Length>3 && value.Trim().Length<30)
+                      { 
+                            name = value.Trim();
+                      }
+            }
+         }
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                if (value.Trim().Length > 2 && value.Trim().Length < 30)
+                 {
+                    surname = value.Trim();
+            }    }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 0 && value < 100)
+                {
+                    age = value;
+                }
+            }
+        }
+        public People(string name, string surname, int age)
+        {
+            Name= name;
+            Surname= surname;
+            Age= age;
+        }
+    }
+   
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Delegate.Models;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        delegate void RegisterEmployee(People people);
+        static void Main(string[] args)
+        {
+            List<People>peoples = new List<People>();
+            People senan = new People("senan", "behbudov ", 19);
+            People samir = new People("Samir", "lutfizade", 30);
+            People xeyale = new People("xeyale", "qehramanova", 17);
+            peoples.Add(senan);
+            peoples.Add(samir);
+            peoples.Add(xeyale);
+            Console.WriteLine("listde axartdiginiz adi daxil edin");
+            string ad = Console.ReadLine(); 
+            Console.WriteLine(peoples.Find(peoples => peoples.Name == ad));
+
+           //foreach(People people in peoples.FindAll(peoples => peoples.Surname.Contains("ov")).Find(peoples=>peoples.surname.Contains("ova")))
+           // {
+           //     Console.WriteLine(peoples);
+           // }
+            Console.WriteLine(peoples.FindAll(peoples=> peoples.age >18));
+            //List<People> NewList = peoples.FindAll(delegate (People P)
+            ////{
+            ////    return (P.Age > 25);
+            ////    Console.WriteLine(P);
+            //});
+            //Console.WriteLine(peoples.FindAll(peoples => peoples.Name.Contains("senan")).Find(peoples => peoples.Name.Length > 0));
+           
+        }
+    }
+}
